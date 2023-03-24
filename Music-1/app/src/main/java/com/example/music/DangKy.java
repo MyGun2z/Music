5bc3b8e63eb002a97de98fe0baa67d199f93556c
MyGun2z <ngochuy29102002@gmail.com>
2023-03-24 22:40:07 +0700

    huy

---
+++ Music-1/app/src/main/java/com/example/music/DangKy.java
@@ -0,0 +1,17 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangKy extends AppCompatActivity {
+
+    ActivityS
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ky);
+    }
+}
